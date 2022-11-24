4987ec97698947ff8b11158f97b69b3cb8685016
Santiago Cabrera <117690415+CabreraSantiago@users.noreply.github.com>
2022-11-23 20:16:52 -0500

    Cabrera Santiago

--- pruebap1/code/cancer.sh
+++ pruebap1/code/cancer.sh
@@ -1,2 +1,5 @@
-echo cancer.sh
+#registros de cada tipo de cáncer
+cat ../data/cancermine.csv | cut -d "," -f 3 | tail -n +2 | sort | uniq -c | sort -n>../data/cancer.txt
 
+#registros del término carcinoma.
+cat ../data/cancermine.csv | cut -d "," -f 3 | tail -n +2 | grep carcinoma | wc -l>../data/carcinoma.txt
